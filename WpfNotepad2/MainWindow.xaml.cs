95f707d7f4deccc0cf5e9318a89b760ebde0e3b2
BitSwapper <killthirster@gmail.com>
2024-10-15 11:24:39 -0400

    minimize working correctly

--- WpfNotepad2/MainWindow.xaml.cs
+++ WpfNotepad2/MainWindow.xaml.cs
@@ -51,14 +51,14 @@ public partial class MainWindow : Window
 
     void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
     {
-        //if(e.ClickCount == 2)
-        //    WindowState = WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
+        if(e.ClickCount == 2)
+            DoWindowMaximizedStateChange();
     }
 
     void Border_MouseMove(object sender, MouseEventArgs e)
     {
         var position = e.GetPosition(this);
-        WindowResizer.ResizeWindow(this, position, UiLayoutConstants.ResizeBorderWidth);
+        WindowResizer.ResizeWindow(this, position, UiLayoutConstants.ResizeBorderWidth * 5);
     }
 
     void MenuItemNew_Click(object sender, RoutedEventArgs e)
@@ -278,7 +278,7 @@ public partial class MainWindow : Window
 
     void btnMaximize_Click(object sender, RoutedEventArgs e)
     {
-        DoWindowStateChange();
+        DoWindowMaximizedStateChange();
     }
 
 
@@ -287,10 +287,17 @@ public partial class MainWindow : Window
     double oldTop;
     double oldWidth;
     double oldHeight;
-    private void Window_StateChanged(object sender, EventArgs e) => DoWindowStateChange();
+    WindowState prevWindowState;
 
-    void DoWindowStateChange()
+    private void Window_StateChanged(object sender, EventArgs e)
     {
+        if(WindowState != WindowState.Minimized) 
+            DoWindowMaximizedStateChange();
+        prevWindowState = WindowState;
+    }
+    void DoWindowMaximizedStateChange()
+    {
+        if(prevWindowState == WindowState.Minimized) return;
         if(!isManuallyMaximized)
         {
             isManuallyMaximized = true;
@@ -319,141 +326,4 @@ public partial class MainWindow : Window
             isManuallyMaximized = false;
         }
     }
-
-
-
-
-
-    //private const int WM_GETMINMAXINFO = 0x0024;
-
-    //[StructLayout(LayoutKind.Sequential)]
-    //public struct MINMAXINFO
-    //{
-    //    public POINT ptReserved;
-    //    public POINT ptMaxSize;
-    //    public POINT ptMaxPosition;
-    //    public POINT ptMinTrackSize;
-    //    public POINT ptMaxTrackSize;
-    //}
-
-    //[StructLayout(LayoutKind.Sequential)]
-    //public struct POINT
-    //{
-    //    public int x;
-    //    public int y;
-    //}
-
-    //[StructLayout(LayoutKind.Sequential)]
-    //public struct MONITORINFO
-    //{
-    //    public int cbSize;
-    //    public RECT rcMonitor;
-    //    public RECT rcWork;
-    //    public uint dwFlags;
-    //}
-
-    //[StructLayout(LayoutKind.Sequential)]
-    //public struct RECT
-    //{
-    //    public int left;
-    //    public int top;
-    //    public int right;
-    //    public int bottom;
-    //}
-
-    //[DllImport("user32.dll")]
-    //static extern IntPtr MonitorFromWindow(IntPtr hwnd, uint dwFlags);
-
-    //[DllImport("user32.dll")]
-    //static extern bool GetMonitorInfo(IntPtr hMonitor, ref MONITORINFO lpmi);
-
-    //[DllImport("user32.dll")]
-    //static extern bool EnumDisplayMonitors(IntPtr hdc, IntPtr lprcClip, MonitorEnumDelegate lpfnEnum, IntPtr dwData);
-
-    //[DllImport("user32.dll", SetLastError = true)]
-    //static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
-
-    //public delegate bool MonitorEnumDelegate(IntPtr hMonitor, IntPtr hdcMonitor, ref RECT lprcMonitor, IntPtr dwData);
-
-    //protected override void OnSourceInitialized(EventArgs e)
-    //{
-    //    base.OnSourceInitialized(e);
-    //    ((HwndSource)PresentationSource.FromVisual(this)).AddHook(WndProc);
-    //}
-
-    //private IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
-    //{
-    //    if(msg == WM_GETMINMAXINFO)
-    //    {
-    //        RECT windowRect;
-    //        GetWindowRect(hwnd, out windowRect);
-
-    //        IntPtr currentMonitor = GetMonitorWithMostOverlap(windowRect);
-
-    //        if(currentMonitor != IntPtr.Zero)
-    //        {
-    //            MONITORINFO monitorInfo = new MONITORINFO();
-    //            monitorInfo.cbSize = Marshal.SizeOf(typeof(MONITORINFO));
-
-    //            if(GetMonitorInfo(currentMonitor, ref monitorInfo))
-    //            {
-    //                RECT workingArea = monitorInfo.rcWork;
-
-    //                MINMAXINFO mmi = (MINMAXINFO)Marshal.PtrToStructure(lParam, typeof(MINMAXINFO));
-    //                mmi.ptMaxSize.x = workingArea.right - workingArea.left;
-    //                mmi.ptMaxSize.y = workingArea.bottom - workingArea.top;
-    //                mmi.ptMaxPosition.x = workingArea.left;
-    //                mmi.ptMaxPosition.y = workingArea.top;
-
-    //                mmi.ptMaxTrackSize.x = mmi.ptMaxSize.x;
-    //                mmi.ptMaxTrackSize.y = mmi.ptMaxSize.y;
-
-    //                Marshal.StructureToPtr(mmi, lParam, true);
-    //                handled = true;
-    //            }
-    //        }
-    //    }
-    //    return IntPtr.Zero;
-    //}
-
-    //private IntPtr GetMonitorWithMostOverlap(RECT windowRect)
-    //{
-    //    IntPtr bestMonitor = IntPtr.Zero;
-    //    int maxArea = 0;
-
-    //    EnumDisplayMonitors(IntPtr.Zero, IntPtr.Zero, (IntPtr hMonitor, IntPtr hdcMonitor, ref RECT monitorRect, IntPtr lParam) =>
-    //    {
-    //        RECT intersection = IntersectRects(windowRect, monitorRect);
-    //        int area = (intersection.right - intersection.left) * (intersection.bottom - intersection.top);
-
-    //        if(area > maxArea)
-    //        {
-    //            maxArea = area;
-    //            bestMonitor = hMonitor;
-    //        }
-
-    //        return true;
-    //    }, IntPtr.Zero);
-
-    //    return bestMonitor;
-    //}
-
-    //private RECT IntersectRects(RECT rect1, RECT rect2)
-    //{
-    //    RECT intersection = new RECT
-    //    {
-    //        left = Math.Max(rect1.left, rect2.left),
-    //        top = Math.Max(rect1.top, rect2.top),
-    //        right = Math.Min(rect1.right, rect2.right),
-    //        bottom = Math.Min(rect1.bottom, rect2.bottom)
-    //    };
-
-    //    // If the rectangles don't intersect, return an empty rectangle
-    //    if(intersection.right < intersection.left || intersection.bottom < intersection.top)
-    //    {
-    //        intersection = new RECT();
-    //    }
-
-    //    return intersection;
-    //}
 }
