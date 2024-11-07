01228f74263413e01e3304d7cf9b454e2c5843b9
BitSwapper <killthirster@gmail.com>
2024-11-06 23:37:40 -0500

    removed settings service

--- WpfNotepad2/Services/Interfaces/ISettingsService.cs
+++
@@ -1,9 +0,0 @@
-﻿using NotepadEx.MVVM.Models;
-
-namespace NotepadEx.Services.Interfaces;
-
-//public interface ISettingsService
-//{
-//    AppSettings LoadSettings();
-//    void SaveSettings(AppSettings settings);
-//}
