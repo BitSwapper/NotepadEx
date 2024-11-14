6bba12ba62d9b7925e79a8e7c1b833520ec709df
BitSwapper <killthirster@gmail.com>
2024-11-14 16:31:06 -0500

    Update BooleanToIconConverter.cs

--- WpfNotepad2/Converters/BooleanToIconConverter.cs
+++ WpfNotepad2/Converters/BooleanToIconConverter.cs
@@ -7,7 +7,7 @@ public class BooleanToIconConverter : IValueConverter
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
     {
         bool isMaximized = (bool)value;
-        return isMaximized ? "🗗" : "🗗"; // Use "🗖" for restore and "🗗" for maximize //Maximize and Overlap are actual names but i can't seem to get overlap to work so i used pc icon lmao
+        return isMaximized ? "🖥" : "🗗"; // Use "🗖" for restore and "🗗" for maximize //Maximize and Overlap are actual unicode names but i can't seem to get overlap to work so i used pc icon lmao
     }
 
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
